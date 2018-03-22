93b75e4fa35e6a48a63f75e77a6fc95843d4df6f
bballew <bari.ballew@nih.gov>
2018-03-22 13:05:47 -0400

    allow unmapped reads to pass filter

--- scripts/filter_bams.sh
+++ scripts/filter_bams.sh
@@ -3,4 +3,5 @@
 inFile=$1
 outFile=$2
 
-samtools view -h $inFile | awk '{FS=OFS="\t"} $7 !~ /\*/ || $0 ~ /^@/{print $0}' | samtools view -b > $outFile
+samtools view -h $inFile | awk '{FS=OFS="\t"} ($7 !~ /\*/ && $2 =~ /65|81|97|113|129|145|161|177/) || $0 ~ /^@/{print $0}' | samtools view -b > $outFile
+# samtools view -h $inFile | awk '{FS=OFS="\t"} $7 !~ /\*/ || $0 ~ /^@/{print $0}' | samtools view -b > $outFile

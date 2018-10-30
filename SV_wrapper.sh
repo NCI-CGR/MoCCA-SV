a662022003cfb7ec3d8dc620ebac39b2c6ed0fc5
Bari Ballew <bari.ballew@nih.gov>
2018-10-30 16:13:25 -0400

    coordinate pipeline via snakemake include

--- SV_wrapper.sh
+++ SV_wrapper.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-#$ -S /bin/sh
-#$ -j y
 
-# to run: qsub -q xlong.q -o /DCEG/Projects/WGS_chordoma/CGR_reanalysis/primary/struct_vars_052518/ SV_wrapper.sh /DCEG/Projects/WGS_chordoma/CGR_reanalysis/primary/struct_vars_052518/config_annotate.yaml
+# to run: 
+# module load python3 sge perl
+# unset module
+# qsub -q xlong.q -V -j y -S /bin/sh -o ${PWD} ${PWD}/SV_wrapper.sh ${PWD}/config.yaml
 
 configFile=$1
 
@@ -14,16 +15,36 @@ fi
 if [ ! -f $configFile ]; then
     echo "Config file not found."
     exit 1
-else
-    # note that this will only work for simple, single-level yaml
-    execDir=$(awk '($0~/^execDir/){print $2}' $configFile | sed "s/'//g")
-    cluster=$(awk '($0~/^clusterMode/){print $2}' $configFile | sed "s/'//g")
-
-    if [ "$cluster" = "SGE" ]; then
-        source /etc/profile.d/modules.sh
-        module load sge perl
-        perl $execDir/SV_wrapper.pl $configFile
-    elif [ "$cluster" = "local" ]; then
-        echo "TODO: write a tiny wrapper to kick off applicable snake jobs assuming one local core"
-    fi
 fi
+
+# note that this will only work for simple, single-level yaml
+# this also requires a whitespace between the key and value pair in the config (except for the cluster command, which requires single or double quotes)
+execDir=$(awk '($0~/^execDir/){print $2}' $configFile | sed "s/'//g")
+
+# insert config file checking script here!
+perl ${execDir}/scripts/check_config.pl $configFile
+
+logDir=$(awk '($0~/^logDir/){print $2}' $configFile | sed "s/'//g") 
+outDir=$(awk '($0~/^outDir/){print $2}' $configFile | sed "s/'//g") 
+numJobs=$(awk '($0~/^maxNumJobs/){print $2}' $configFile | sed "s/'//g")
+clusterLine=$(awk '($0~/^clusterMode/){print $0}' $configFile | sed "s/\"/'/g")  # allows single or double quoting of the qsub command in the config file
+clusterMode='"'$(echo $clusterLine | awk -F\' '($0~/^clusterMode/){print $2}')'"'
+
+
+
+if [ ! -d $logDir ]; then
+    mkdir -p $logDir
+fi
+
+DATE=$(date +"%Y%m%d%H%M")
+
+if [ "$clusterMode" = "local" ]; then
+    cmd="conf=$configFile snakemake -p -s ${execDir}/Snakefile_SV_wrapper --rerun-incomplete &> ${logDir}/MoCCA-SV_${DATE}.out"
+# elif [ "$cluster" = "unlock" ]  # put in a convenience unlock?
+#   cmd="conf=$configFile snakemake -p -s ${execDir}/Snakefile_SV_wrapper --unlock"
+else
+    cmd="conf=$configFile snakemake -p -s ${execDir}/Snakefile_SV_wrapper --rerun-incomplete --cluster ${clusterMode} --jobs $numJobs --latency-wait 300 &> ${logDir}/MoCCA-SV_${DATE}.out"
+fi
+
+echo "Command run: $cmd"
+eval $cmd  # exec $cmd

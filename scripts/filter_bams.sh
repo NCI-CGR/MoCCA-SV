ccb8e8ba9da64d40f0b1dd8aa0651f5d4f756a94
Bari Ballew <bari.ballew@nih.gov>
2019-04-03 16:17:31 -0400

    more bam flag filtering for manta

--- scripts/filter_bams.sh
+++ scripts/filter_bams.sh
@@ -3,5 +3,6 @@
 inFile=$1
 outFile=$2
 
-samtools view -h $inFile | awk '{FS=OFS="\t"} ($7 ~ /\*/ && $2 ~ /65|81|97|113|129|145|161|177/) || $7 !~ /\*/ || $0 ~ /^@/ {print $0}' | samtools view -b > $outFile
-# samtools view -h $inFile | awk '{FS=OFS="\t"} $7 !~ /\*/ || $0 ~ /^@/{print $0}' | samtools view -b > $outFile
+# samtools view -h $inFile | awk '{FS=OFS="\t"} ($7 ~ /\*/ && $2 ~ /65|81|97|113|129|145|161|177/) || $7 !~ /\*/ || $0 ~ /^@/ {print $0}' | samtools view -b > $outFile
+    # for whatever reason, reads with the above flags still choke in manta
+samtools view -h $inFile | awk '{FS=OFS="\t"} $7 !~ /\*/ || $0 ~ /^@/{print $0}' | samtools view -b > $outFile

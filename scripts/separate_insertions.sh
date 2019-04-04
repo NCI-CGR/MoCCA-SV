cfb252d2cfe45294951954bf06d4d3a5b8cff210
Bari Ballew <bari.ballew@nih.gov>
2019-04-04 14:17:01 -0400

    handle no insertions <10bp

--- scripts/separate_insertions.sh
+++ scripts/separate_insertions.sh
@@ -5,5 +5,12 @@ outFile1=$2
 outFile2=$3
 
 
+i=0
+
 # separate out SVs spanning <=10bp
-awk -v out1=$outFile1 -v out2=$outFile2 '{FS=OFS="\t"} ($3-$2<=10){print $0 > "out1"} else {print $0 > "out2"}' $inFile
+awk -v out1="$outFile1" -v out2="$outFile2" -v counter="$i" '{FS=OFS="\t"} {if($3-$2<=10) {print $0 > out1; counter++} else {print $0 > out2}}' $inFile
+
+# handle case of no SVs <=10bp
+if [ $i -eq 0 ]; then
+    touch $outFile1
+fi

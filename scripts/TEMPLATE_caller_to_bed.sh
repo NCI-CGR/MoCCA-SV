24decb1e64d4472ce78b519e38ee674becf32454
bballew <bari.ballew@nih.gov>
2019-04-09 14:16:09 -0400

    more explanation of BND/other notation

--- scripts/TEMPLATE_caller_to_bed.sh
+++ scripts/TEMPLATE_caller_to_bed.sh
@@ -3,7 +3,8 @@
 input=$1
 sample=$2
 outDir=$3
-varType="BND"  # how to differentiate interchromosomal translocations (with four breakends) from insertions/deletions/duplications (two breakends)
+varType="BND"   # how to differentiate interchromosomal translocations (with four breakends) from insertions/deletions/duplications (two breakends)
+                # this will change depending on caller output - see other <caller>_to_bed.sh scripts for examples
 
 <insert command line to transform data to bed here> > $outDir$sample"_intra.bed"
 <insert command line to transform data to bed here> > $outDir$sample"_end1.bed"

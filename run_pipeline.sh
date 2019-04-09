7b0bf61353614dc465770a8a3e345db0845d4826
bballew <bari.ballew@nih.gov>
2019-04-09 14:10:15 -0400

    minor cleanup

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -2,6 +2,6 @@
 
 module load python3/3.7.0 sge perl singularity/3.0.1
 unset module
-cmd="qsub -q research.q -V -j y -S /bin/sh -o ${PWD} ${PWD}/SV_wrapper.sh ${PWD}/config.yaml"
+cmd="qsub -q long.q -V -j y -S /bin/sh -o ${PWD} ${PWD}/SV_wrapper.sh ${PWD}/config.yaml"
 echo "Command run: $cmd"
 eval $cmd

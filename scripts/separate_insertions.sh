6f6cd1a09d7f95276384f400a9089965afa4a7b5
Bari Ballew <bari.ballew@nih.gov>
2019-05-14 15:31:34 -0400

    account for 0 SVs found

--- scripts/separate_insertions.sh
+++ scripts/separate_insertions.sh
@@ -6,11 +6,17 @@ outFile2=$3
 
 
 i=0
+j=0
 
 # separate out SVs spanning <=10bp
-awk -v out1="$outFile1" -v out2="$outFile2" -v counter="$i" '{FS=OFS="\t"} {if($3-$2<=10) {print $0 > out1; counter++} else {print $0 > out2}}' $inFile
+awk -v out1="$outFile1" -v out2="$outFile2" -v i="$i" -v j="$j" '{FS=OFS="\t"} {if($3-$2<=10) {print $0 > out1; i++} else {print $0 > out2; j++}}' $inFile
 
 # handle case of no SVs <=10bp
 if [ $i -eq 0 ]; then
     touch $outFile1
+fi
+
+# handle case of no SVs >10 bp
+if [ $j -eq 0 ]; then
+    touch $outFile2
 fi

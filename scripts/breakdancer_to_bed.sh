20111abb97896937478e8c555d59b8652623e965
Bari Ballew <bari.ballew@nih.gov>
2019-05-23 11:20:18 -0400

    fix bed conversion if pos1==0

--- scripts/breakdancer_to_bed.sh
+++ scripts/breakdancer_to_bed.sh
@@ -6,11 +6,11 @@ outDir=$3
 varType="CTX"
 
 if [[ $input = *.gz ]]; then
-    zcat $input | awk '{FS=OFS="\t"}{print $0, NR}' | grep -v "^#" | grep -E "\s$varType\s" | awk '{FS=OFS="\t"} {print $1, $2 - 1, $2, $NF}' > $outDir$sample"_end1.bed"
+    zcat $input | awk '{FS=OFS="\t"}{print $0, NR}' | grep -v "^#" | grep -E "\s$varType\s" | awk '{FS=OFS="\t"} ($2!=0){print $1, $2 - 1, $2, $NF} ($2==0){print $1, $2, $2+1, $NF}' > $outDir$sample"_end1.bed"
     zcat $input | awk '{FS=OFS="\t"}{print $0, NR}' | grep -v "^#" | grep -E "\s$varType\s" | awk '{FS=OFS="\t"} {print $4, $5 - 1, $5, $NF}' > $outDir$sample"_end2.bed"
     zcat $input | awk '{FS=OFS="\t"}{print $0, NR}' | grep -v "^#" | grep -vE "\s$varType\s" | awk '{FS=OFS="\t"} {print $1, $2, $5, $NF}' | awk '{FS=OFS="\t"} ($2<$3){print $0} ($2>$3){print $1,$3,$2,$4} ($2==$3){print $1,$2,$3+1,$4}' >> $outDir$sample"_intra.bed"
 else
-    awk '{FS=OFS="\t"}{print $0, NR}' $input | grep -v "^#" | grep -E "\s$varType\s" | awk '{FS=OFS="\t"} {print $1, $2 - 1, $2, $NF}' > $outDir$sample"_end1.bed"
+    awk '{FS=OFS="\t"}{print $0, NR}' $input | grep -v "^#" | grep -E "\s$varType\s" | awk '{FS=OFS="\t"} ($2!=0){print $1, $2 - 1, $2, $NF} ($2==0){print $1, $2, $2+1, $NF}' > $outDir$sample"_end1.bed"
     awk '{FS=OFS="\t"}{print $0, NR}' $input | grep -v "^#" | grep -E "\s$varType\s" | awk '{FS=OFS="\t"} {print $4, $5 - 1, $5, $NF}' > $outDir$sample"_end2.bed"
     awk '{FS=OFS="\t"}{print $0, NR}' $input | grep -v "^#" | grep -vE "\s$varType\s" | awk '{FS=OFS="\t"} {print $1, $2, $5, $NF}' | awk '{FS=OFS="\t"} ($2<$3){print $0} ($2>$3){print $1,$3,$2,$4} ($2==$3){print $1,$2,$3+1,$4}' >> $outDir$sample"_intra.bed"
 fi
--- scripts/svaba_to_bed.sh
+++ scripts/svaba_to_bed.sh
@@ -7,10 +7,10 @@ varType="SPAN=-1"
 
 if [[ $input = *.gz ]]; then
     zcat $input | awk '{FS=OFS="\t"}{print $0, NR}' | grep -v "^#" | grep -v $varType | awk '{FS=OFS="\t"} {print $1, $2, gensub(/.*[[:blank:]][atcgnNATCG]*[\[\]][chrGLhs37d]*[0-9MTXY.]+:([0-9]+)[\[\]].*/, "\\1", "g"), $NF}' | awk '{FS=OFS="\t"} ($2<$3){print $0} ($2>$3){print $1,$3,$2,$4} ($2==$3){print $1,$2,$3+1,$4}' > $outDir$sample"_intra.bed"
-    zcat $input | awk '{FS=OFS="\t"}{print $0, NR}' | grep -v "^#" | grep $varType | awk '{FS=OFS="\t"} {print $1, $2 - 1, $2, $NF}' > $outDir$sample"_end1.bed"
+    zcat $input | awk '{FS=OFS="\t"}{print $0, NR}' | grep -v "^#" | grep $varType | awk '{FS=OFS="\t"} ($2!=0){print $1, $2 - 1, $2, $NF} ($2==0){print $1, $2, $2+1, $NF}' > $outDir$sample"_end1.bed"
     zcat $input | awk '{FS=OFS="\t"}{print $0, NR}' | grep -v "^#" | grep $varType | awk '{FS=OFS="\t"} {print gensub(/.*[[:blank:]][atcgnNATCG]*[\[\]]([chrGLhs37d]*[0-9MTXY.]+):[0-9]+[\[\]].*/, "\\1", "g"), gensub(/.*[[:blank:]][atcgnNATCG]*[\[\]][chrGLhs37d]*[0-9MTXY.]+:([0-9]+)[\[\]].*/, "\\1", "g") - 1, gensub(/.*[[:blank:]][atcgnNATCG]*[\[\]][chrGLhs37d]*[0-9MTXY.]+:([0-9]+)[\[\]].*/, "\\1", "g"), $NF}' > $outDir$sample"_end2.bed"    
 else
     awk '{FS=OFS="\t"}{print $0, NR}' $input | grep -v "^#" | grep -v $varType | awk '{FS=OFS="\t"} {print $1, $2, gensub(/.*[[:blank:]][atcgnNATCG]*[\[\]][chrGLhs37d]*[0-9MTXY.]+:([0-9]+)[\[\]].*/, "\\1", "g"), $NF}' | awk '{FS=OFS="\t"} ($2<$3){print $0} ($2>$3){print $1,$3,$2,$4} ($2==$3){print $1,$2,$3+1,$4}' > $outDir$sample"_intra.bed"
-    awk '{FS=OFS="\t"}{print $0, NR}' $input | grep -v "^#" | grep $varType | awk '{FS=OFS="\t"} {print $1, $2 - 1, $2, $NF}' > $outDir$sample"_end1.bed"
+    awk '{FS=OFS="\t"}{print $0, NR}' $input | grep -v "^#" | grep $varType | awk '{FS=OFS="\t"} ($2!=0){print $1, $2 - 1, $2, $NF} ($2==0){print $1, $2, $2+1, $NF}' > $outDir$sample"_end1.bed"
     awk '{FS=OFS="\t"}{print $0, NR}' $input | grep -v "^#" | grep $varType | awk '{FS=OFS="\t"} {print gensub(/.*[[:blank:]][atcgnNATCG]*[\[\]]([chrGLhs37d]*[0-9MTXY.]+):[0-9]+[\[\]].*/, "\\1", "g"), gensub(/.*[[:blank:]][atcgnNATCG]*[\[\]][chrGLhs37d]*[0-9MTXY.]+:([0-9]+)[\[\]].*/, "\\1", "g") - 1, gensub(/.*[[:blank:]][atcgnNATCG]*[\[\]][chrGLhs37d]*[0-9MTXY.]+:([0-9]+)[\[\]].*/, "\\1", "g"), $NF}' > $outDir$sample"_end2.bed"
 fi
